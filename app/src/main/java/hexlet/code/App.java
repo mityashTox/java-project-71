9c7ac5cf6a9b1a96fa6719009c2dc8162f33f3c8
toxigen <mityashin.dima@gmail.com>
2023-09-15 19:11:52 +1000

    add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,12 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "./app/build/install/app/bin/app",
+        version = "2.0",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
